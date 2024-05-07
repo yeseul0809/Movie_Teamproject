4839a0e462200bdf35d909e980949ae34fa55fc3
leeboa2005 <leeboa2005@naver.com>
2024-05-07 18:33:19 +0900

    리뷰 페이지 레이아웃 수정

--- modules/review.js
+++ modules/review.js
@@ -1,13 +1,13 @@
 // 상단 구조
 const url = new URLSearchParams(window.location.search);
-const movieId = url.get('id');
+const movieId = url.get("id");
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjVhY2I0YWI1YjY1MzRlYjlhMTY5MmI5MWFlNjQ5MiIsInN1YiI6IjY2Mjc1N2Q3MTk3ZGU0MDE3ZDJkNTU0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._21B2bQFep_OOlQFiZX23F8LcMguJbtnrZLuaRRFMbc',
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjVhY2I0YWI1YjY1MzRlYjlhMTY5MmI5MWFlNjQ5MiIsInN1YiI6IjY2Mjc1N2Q3MTk3ZGU0MDE3ZDJkNTU0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._21B2bQFep_OOlQFiZX23F8LcMguJbtnrZLuaRRFMbc",
   },
 };
 
@@ -45,12 +45,12 @@ fetchMoviesData();
 
 function makeDesc(data) {
   document.querySelector(
-    '#movie-poster'
+    "#movie-poster"
   ).style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${data.poster_path})`;
-  document.getElementById('movie-title').textContent = data.title;
-  document.getElementById('movie-description').textContent = data.overview;
+  document.getElementById("movie-title").textContent = data.title;
+  document.getElementById("movie-description").textContent = data.overview;
   document.getElementById(
-    'movie-rating'
+    "movie-rating"
   ).textContent = `Rating : ${data.vote_average}`;
 }
 
@@ -80,49 +80,49 @@ function getFilteredMoviesData(data) {
   return filterMovies;
 }
 
-const noRelatedWarning = document.querySelector('.noRelatedWarning');
+const noRelatedWarning = document.querySelector(".noRelatedWarning");
 
 function makeRelatedMovies(filterMovies) {
   if (filterMovies.length === 0) {
-    noRelatedWarning.classList.remove('hidden');
+    noRelatedWarning.classList.remove("hidden");
   }
-  const recommendList = document.querySelector('#recommend-list');
+  const recommendList = document.querySelector("#recommend-list");
   filterMovies.forEach((movie) => {
-    const list = document.createElement('li');
-    list.classList.add('relatedList');
+    const list = document.createElement("li");
+    list.classList.add("relatedList");
     list.innerHTML = `<img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="${movie.title}">`;
     recommendList.appendChild(list);
 
-    list.addEventListener('click', () => {
+    list.addEventListener("click", () => {
       window.location.href = `review.html?id=${movie.id}`;
     });
   });
 }
 
 // 리뷰 관련 기능
-const reviewForm = document.getElementById('review-form');
-const reviewList = document.getElementById('review-list');
+const reviewForm = document.getElementById("review-form");
+const reviewList = document.getElementById("review-list");
 
 // 로컬 스토리지에서 리뷰 데이터 가져오기
-let reviews = JSON.parse(localStorage.getItem('reviews')) || {};
+let reviews = JSON.parse(localStorage.getItem("reviews")) || {};
 reviews[movieId] = reviews[movieId] || []; //리뷰데이터 초기화
 
 displayReviews(movieId);
 
 // 리뷰 작성 및 저장
-reviewForm.addEventListener('submit', (event) => {
+reviewForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const reviewText = document.getElementById('review-text').value.trim(); //공백 제거 후 입력값 할당.
+  const reviewText = document.getElementById("review-text").value.trim(); //공백 제거 후 입력값 할당.
   const reviewPassword = document
-    .getElementById('review-password')
+    .getElementById("review-password")
     .value.trim();
 
   // 비밀번호 입력 확인
   if (!reviewPassword) {
     Swal.fire({
-      text: '비밀번호를 영어 대문자를 포함하여 8자 이상 작성해주세요.',
-      icon: 'warning',
-      confirmButtonColor: 'gray',
+      text: "비밀번호를 영어 대문자를 포함하여 8자 이상 작성해주세요.",
+      icon: "warning",
+      confirmButtonColor: "gray",
     });
     return;
   }
@@ -130,9 +130,9 @@ reviewForm.addEventListener('submit', (event) => {
   // 비밀번호 유효성 검사
   if (reviewPassword.length < 8 || !/[A-Z]/.test(reviewPassword)) {
     Swal.fire({
-      text: '비밀번호는 영어 대문자를 포함하여 8자 이상 작성해야합니다.',
-      icon: 'warning',
-      confirmButtonColor: 'gray',
+      text: "비밀번호는 영어 대문자를 포함하여 8자 이상 작성해야합니다.",
+      icon: "warning",
+      confirmButtonColor: "gray",
     });
     return;
   }
@@ -145,32 +145,35 @@ reviewForm.addEventListener('submit', (event) => {
       password: reviewPassword,
     };
     reviews[movieId].push(newReview); //ID별 새로운 리뷰데이터 추가
-    localStorage.setItem('reviews', JSON.stringify(reviews));
+    localStorage.setItem("reviews", JSON.stringify(reviews));
 
-    document.getElementById('review-text').value = '';
-    document.getElementById('review-password').value = '';
+    document.getElementById("review-text").value = "";
+    document.getElementById("review-password").value = "";
     displayReviews(movieId);
   }
 });
 
 // 리뷰 목록 표시
 function displayReviews(movieId) {
-  reviewList.innerHTML = '';
+  reviewList.innerHTML = "";
   reviews[movieId].forEach((review, index) => {
-    const reviewItem = document.createElement('li');
+    const reviewItem = document.createElement("li");
     reviewItem.textContent = `${review.text} (${review.timestamp})`;
+    const buttonWrap = document.createElement("div");
+    buttonWrap.classList.add("review_btn_wrap");
+    reviewItem.append(buttonWrap);
 
     //수정 버튼 추가
-    const editButton = document.createElement('button');
-    editButton.textContent = '수정';
-    editButton.addEventListener('click', () => editReview(movieId, index)); // 클릭시 리뷰 수정 함수 호출
-    reviewItem.appendChild(editButton);
+    const editButton = document.createElement("button");
+    editButton.textContent = "modify";
+    editButton.addEventListener("click", () => editReview(movieId, index)); // 클릭시 리뷰 수정 함수 호출
+    buttonWrap.appendChild(editButton);
 
     //삭제 버튼 추가
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = '삭제';
-    deleteButton.addEventListener('click', () => deleteReview(movieId, index)); // 클릭시 리뷰 삭제 함수 호출
-    reviewItem.appendChild(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    deleteButton.addEventListener("click", () => deleteReview(movieId, index)); // 클릭시 리뷰 삭제 함수 호출
+    buttonWrap.appendChild(deleteButton);
 
     reviewList.appendChild(reviewItem);
   });
@@ -179,37 +182,37 @@ function displayReviews(movieId) {
 // 리뷰 수정
 function editReview(movieId, index) {
   Swal.fire({
-    title: '비밀번호를 입력하세요',
-    input: 'password',
+    title: "비밀번호를 입력하세요",
+    input: "password",
     showCancelButton: true,
-    confirmButtonText: '확인',
-    cancelButtonText: '취소',
-    cancelButtonColor: 'gray',
+    confirmButtonText: "확인",
+    cancelButtonText: "취소",
+    cancelButtonColor: "gray",
 
     //확인 버튼 클릭시 실행
     preConfirm: (password) => {
       if (password === reviews[movieId][index].password) {
         return Swal.fire({
-          title: '수정할 리뷰를 입력하세요',
-          input: 'textarea',
+          title: "수정할 리뷰를 입력하세요",
+          input: "textarea",
           inputValue: reviews[movieId][index].text,
           showCancelButton: true,
-          confirmButtonText: '저장',
-          cancelButtonText: '취소',
+          confirmButtonText: "저장",
+          cancelButtonText: "취소",
           preConfirm: (newText) => {
             if (newText != null) {
               reviews[movieId][index].text = newText;
-              localStorage.setItem('reviews', JSON.stringify(reviews));
+              localStorage.setItem("reviews", JSON.stringify(reviews));
               displayReviews(movieId);
-              Swal.fire('수정완료', '리뷰가 수정되었습니다.', 'success');
+              Swal.fire("수정완료", "리뷰가 수정되었습니다.", "success");
             }
           },
         });
       } else {
         Swal.fire({
-          text: '비밀번호가 일치하지 않습니다.',
-          icon: 'error',
-          confirmButtonColor: 'gray',
+          text: "비밀번호가 일치하지 않습니다.",
+          icon: "error",
+          confirmButtonColor: "gray",
         });
       }
     },
@@ -219,38 +222,38 @@ function editReview(movieId, index) {
 // 리뷰 삭제
 function deleteReview(movieId, index) {
   Swal.fire({
-    title: '비밀번호를 입력하세요',
-    input: 'password',
+    title: "비밀번호를 입력하세요",
+    input: "password",
     showCancelButton: true,
-    confirmButtonColor: 'red',
-    cancelButtonColor: 'gray',
-    cancelButtonText: '취소',
-    confirmButtonText: '확인',
+    confirmButtonColor: "red",
+    cancelButtonColor: "gray",
+    cancelButtonText: "취소",
+    confirmButtonText: "확인",
     preConfirm: (password) => {
       if (password === reviews[movieId][index].password) {
         reviews[movieId].splice(index, 1);
-        localStorage.setItem('reviews', JSON.stringify(reviews));
+        localStorage.setItem("reviews", JSON.stringify(reviews));
         displayReviews(movieId);
-        Swal.fire('삭제완료', '리뷰가 삭제되었습니다.', 'success');
+        Swal.fire("삭제완료", "리뷰가 삭제되었습니다.", "success");
       } else {
         Swal.fire({
-          text: '비밀번호가 일치하지 않습니다.',
-          icon: 'error',
-          confirmButtonColor: 'gray',
+          text: "비밀번호가 일치하지 않습니다.",
+          icon: "error",
+          confirmButtonColor: "gray",
         });
       }
     },
   });
 }
 
-const prevRelateBtn = document.querySelector('.relatePrevBtn');
-const nextRelateBtn = document.querySelector('.relateNextBtn');
-const relateWrap = document.querySelector('#recommend-list');
+const prevRelateBtn = document.querySelector(".relatePrevBtn");
+const nextRelateBtn = document.querySelector(".relateNextBtn");
+const relateWrap = document.querySelector("#recommend-list");
 
 let current = 0;
 
 function moveRelateSlide(direction) {
-  const relateSlide = document.querySelector('.relatedList');
+  const relateSlide = document.querySelector(".relatedList");
   const slideWidth = relateSlide?.offsetWidth;
   let newLeft = current;
 
@@ -268,11 +271,11 @@ function moveRelateSlide(direction) {
     newLeft = minLeft;
   }
 
-  relateWrap.style.transition = '300ms';
+  relateWrap.style.transition = "300ms";
   relateWrap.style.left = `${newLeft}px`;
 
   current = newLeft;
 }
 
-prevRelateBtn.addEventListener('click', () => moveRelateSlide(-1));
-nextRelateBtn.addEventListener('click', () => moveRelateSlide(1));
+prevRelateBtn.addEventListener("click", () => moveRelateSlide(-1));
+nextRelateBtn.addEventListener("click", () => moveRelateSlide(1));
